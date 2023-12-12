b7ab2ef88e76c60ff4cb57dc78eed046d1225808
Your Name <you@example.com>
2023-12-12 18:11:11 +0200

    Sign up done

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const mysql = require("mysql");
 const cors = require("cors");
 const axios = require("axios");
 const bodyParser = require("body-parser")
+const util = require('util');
+
+
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,10 +24,10 @@ app.get('/', (req,res)=>{
 })
 
 app.get('/members', (req,res)=>{
-    const sql = "SELECT * FROM Members";
+    const sql = "CALL SelectAllMembers";
     db.query(sql, (err,data)=>{
         if(err) return res.json(err);
-        return res.json(data);
+        return res.json(data[0]);
     })
 })
 app.get('/teams', (req,res)=>{
@@ -54,7 +57,7 @@ app.post('/login', (req,res)=>{
         if(err) return res.json(err);
         else{
           
-            console.log(data[0].PasswordM);
+            
             if(password === data[0].PasswordM)
             {
                 console.log('User successfully logged in');
@@ -68,6 +71,43 @@ app.post('/login', (req,res)=>{
         }
         
     })
+})
+
+app.post('/signup',async (req,res)=>{
+    console.log(req.body);
+    const email = req.body['email'];
+    const password = req.body['password'];
+    const name = req.body['name'];
+    const major = req.body['major'];
+    const gradYear = req.body['gradYear'];
+    const position = 'member';
+    const dbQueryAsync = util.promisify(db.query).bind(db);
+
+
+    inUse = false;
+    const emailCheck = `SELECT * FROM members WHERE Email ="${email}" `;
+
+   try {
+    const data = await dbQueryAsync(emailCheck);
+    console.log('executing first query');
+
+    if (data.length > 0) {
+        console.log("email already in use");
+        inUse = true;
+        console.log(inUse, data);
+        return res.json(false);
+    }
+
+    const sql = `INSERT INTO members (nameM,Admin_ssn,Position,Major,Email,PasswordM,gradyear) VALUES ('${name}',1,'${position}','${major}','${email}','${password}','${gradYear}')`;
+
+    if (!inUse) {
+        const insertData = await dbQueryAsync(sql);
+        console.log("1 row inserted into 'members'");
+        return res.json(true);
+    }
+} catch (err) {
+    return res.json(err);
+}
 })
 
 app.listen(8081, ()=>{
