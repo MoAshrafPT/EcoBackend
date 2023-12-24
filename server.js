9769f13e434f302d9f1886ab0c87fe3e1bfcd57d
MoAshrafPT <121402445+MoAshrafPT@users.noreply.github.com>
2023-12-24 18:19:03 +0200

    queries added

--- server.js
+++ server.js
@@ -45,6 +45,133 @@ app.get('/teams', (req,res)=>{
         return res.json(data);
     })
 })
+app.get('/tools',(req,res)=>{
+    const sql = "SELECT * FROM Tools";
+    db.query(sql, (err,data)=>{
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+})
+app.get('/sponsors', (req,res)=>{
+    const sql = "SELECT * FROM sponsors";
+    db.query(sql, (err,data)=>{
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+})
+app.get('/incidents', (req,res)=>{
+    const sql = "SELECT * FROM race_incidents";
+    db.query(sql, (err,data)=>{
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+})
+
+app.get('/alumni', (req,res)=>{
+    const sql = "SELECT * FROM aumini";
+    db.query(sql, (err,data)=>{
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+})
+
+app.get('/projects', (req,res)=>{
+    const sql = "SELECT * FROM projects";
+    db.query(sql, (err,data)=>{
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+})
+
+app.get('/races', (req,res)=>{
+    const sql = "SELECT * FROM races";
+    db.query(sql, (err,data)=>{
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+})
+app.get('/admins' , (req,res)=>{
+    const sql = "SELECT * FROM administrators";
+    db.query(sql, (err,data)=>{
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+})
+
+app.get('/funds',(req,res)=>{
+    const sql = `SELECT * FROM funds WHERE nameF = ${req.body.name}`;
+    db.query(sql, (err,data)=>{
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+})
+app.get('/myrequests/:id',(req,res)=>{
+    const sql = `SELECT * FROM tool_requests WHERE memberID = ${req.params.id}`;
+    db.query(sql, (err,data)=>{
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+})
+
+app.get('/allrequests',(req,res)=>{
+    const sql = `SELECT * FROM tool_requests`;
+    db.query(sql, (err,data)=>{
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+})
+
+app.patch('/updateinfo',(req,res)=>{
+    
+    //TODO: user can change his password, email, major
+    const newEmail = '';
+    const newPassword = '';
+    const newMajor = '';
+    const sql = `UPDATE members SET `
+    if(newEmail !== '')
+    {
+        sql+= `Email = ${newEmail},`
+    }
+    if(newPassword !== '')
+    {
+        sql+= `PasswordM = ${newPassword},`
+    }
+    if(newMajor !== ''){
+        sql+= `Major = ${newMajor},`
+    }
+    sql= sql.slice(0,-1);
+    sql+= `WHERE Mid = ${req.body.Mid}`;
+    db.query(sql, (err,data)=>{
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+
+   
+});
+
+app.post('/feedback',(req,res)=>{
+    //TODO: user gives feedback on a particular member
+})
+app.post('/requesttools',(req,res)=>{
+    //TODO: handle request data coming from form
+    const toolName = req.body.tool;
+    const quantity = req.body.quantity;
+    const ddl = req.body.deadline;
+    const id = req.body.memberID;
+
+    const sql = `INSERT INTO tool_requests (memberID, quantity, deadline, toolname) VALUES (${id},${quantity},'${ddl}','${toolName}')`
+    db.query(sql, (err,data)=>{
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+
+})
+app.post('/discipline',(req,res)=>{         //disciplinary action against users
+
+})
+
+
+
 
 
 
@@ -54,7 +181,7 @@ app.post('/login', (req,res)=>{
     const password = req.body['password'];
     
     
-    const sql = `SELECT PasswordM , nameM, Mid FROM Members WHERE Email = "${email}"`;
+    const sql = `SELECT PasswordM , nameM, Mid, Position FROM Members WHERE Email = "${email}"`;
     db.query(sql, (err,data)=>{
         if(err) return res.json(err);
         else{
@@ -64,7 +191,7 @@ app.post('/login', (req,res)=>{
             {
                 console.log('User successfully logged in');
                 console.log(data[0].nameM);
-                return res.json({data:true, row:data[0].nameM,id:data[0].Mid});
+                return res.json({data:true, row:data[0].nameM,id:data[0].Mid,role:data[0].Position});
             }
             else{
                 console.log("Password does not match");
